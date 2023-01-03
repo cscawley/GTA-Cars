4d18938ac4879a87375a82e8e1190367602da007
cscawley <cscawley@mac.com>
2023-01-02 20:32:20 -0600

    Mercedes-Benz S 600

---
+++ Benz_S600/fxmanifest.lua
@@ -0,0 +1,15 @@
+fx_version 'cerulean'
+game 'common'
+files {
+  'data/handling.meta',
+  'data/vehicles.meta',
+  'data/vehiclelayouts.meta',
+  'data/carvariations.meta',
+  'data/carcols.meta',
+}
+
+data_file 'HANDLING_FILE' 'data/handling.meta'
+data_file 'VEHICLE_LAYOUTS_FILE' 'data/vehiclelayouts.meta'
+data_file 'CARCOLS_FILE' 'data/carcols.meta'
+data_file 'VEHICLE_METADATA_FILE' 'data/vehicles.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'data/carvariations.meta'
