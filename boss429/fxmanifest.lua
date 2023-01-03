583a6d4294c6ad3c4858c54b0a91c5436b55c41e
cscawley <cscawley@mac.com>
2023-01-02 20:32:59 -0600

    boss429

---
+++ boss429/fxmanifest.lua
@@ -0,0 +1,15 @@
+fx_version 'cerulean'
+game 'common'
+files {
+    'data/handling.meta',
+    'vehiclelayouts.meta',
+    'data/vehicles.meta',
+    'data/carvariations.meta',
+    'data/carcols.meta',
+}
+
+data_file 'HANDLING_FILE' 'data/handling.meta'
+data_file 'VEHICLE_LAYOUTS_FILE' 'vehiclelayouts.meta'
+data_file 'VEHICLE_METADATA_FILE' 'data/vehicles.meta'
+data_file 'CARCOLS_FILE' 'data/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'data/carvariations.meta'
